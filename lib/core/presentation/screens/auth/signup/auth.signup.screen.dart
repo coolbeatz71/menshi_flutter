42c2f2055693cec330d45a42b61f5bbbab5a5bf4
coolbeatz71 <sigmacool@gmail.com>
2024-11-24 18:28:07 +0200

    create sign-up modal widget and screen page

---
+++ lib/core/presentation/screens/auth/signup/auth.signup.screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../../widgets/modal/signup.modal.widget.dart';
+import '../../../widgets/onboarding/onboarding.background.widget.dart';
+
+class AuthSignUpScreen extends StatefulWidget {
+  const AuthSignUpScreen({super.key});
+
+  @override
+  State<AuthSignUpScreen> createState() => _AuthSignUpScreenState();
+}
+
+class _AuthSignUpScreenState extends State<AuthSignUpScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) async {
+        await showGeneralDialog<Widget>(
+          context: context,
+          barrierDismissible: false,
+          pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+            return const SignUpModal();
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const OnboardingBackground();
+  }
+}
--- lib/core/presentation/widgets/onboarding/onboarding.container.widget.dart
+++ lib/core/presentation/widgets/onboarding/onboarding.container.widget.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:menshi/core/utils/colors.dart';
 
+import '../../../utils/colors.dart';
 import '../../../constants/app.dart';
 import '../../entities/onboarding/onboarding.dart';
+import '../../screens/auth/signup/auth.signup.screen.dart';
 import '../../widgets/onboarding/onboarding.decoration.widget.dart';
 import '../shared/animations/fade.animation.dart';
 
@@ -69,8 +70,7 @@ class _OnboardingContainerState extends State<OnboardingContainer> {
                       onPageChanged: (int index) {
                         setState(() {
                           currentIndex = index;
-                          percentage =
-                              (index + 1) / onboardingContentList.length;
+                          percentage = (index + 1) / onboardingContentList.length;
                         });
                       },
                       itemCount: onboardingContentList.length,
@@ -108,10 +108,7 @@ class _OnboardingContainerState extends State<OnboardingContainer> {
                                   child: Text(
                                     onboardingContentList[index].title,
                                     textAlign: TextAlign.center,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineSmall
-                                        ?.copyWith(
+                                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
                                           color: textColor,
                                           fontWeight: FontWeight.w700,
                                         ),
@@ -124,10 +121,7 @@ class _OnboardingContainerState extends State<OnboardingContainer> {
                                   child: Text(
                                     onboardingContentList[index].description,
                                     textAlign: TextAlign.center,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium
-                                        ?.copyWith(
+                                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                                           color: textColor.withOpacity(0.65),
                                           fontWeight: FontWeight.w600,
                                         ),
@@ -176,7 +170,12 @@ class _OnboardingContainerState extends State<OnboardingContainer> {
                               vertical: 12.0,
                             ),
                             onPressed: () {
-                              // Handle finish action
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const AuthSignUpScreen(),
+                                ),
+                              );
                             },
                             child: Container(
                               alignment: Alignment.center,
@@ -188,10 +187,7 @@ class _OnboardingContainerState extends State<OnboardingContainer> {
                               ),
                               child: Text(
                                 'Get Started',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyLarge
-                                    ?.copyWith(
+                                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
                                       color: Colors.white,
                                       fontWeight: FontWeight.w500,
                                     ),
@@ -208,14 +204,16 @@ class _OnboardingContainerState extends State<OnboardingContainer> {
                                 vertical: 12.0,
                               ),
                               onPressed: () {
-                                // Handle skip action
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const AuthSignUpScreen(),
+                                  ),
+                                );
                               },
                               child: Text(
                                 'Skip',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyLarge
-                                    ?.copyWith(
+                                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
                                       color: textColor.withOpacity(0.75),
                                       fontWeight: FontWeight.w500,
                                     ),
@@ -227,8 +225,7 @@ class _OnboardingContainerState extends State<OnboardingContainer> {
                                 vertical: 12.0,
                               ),
                               onPressed: () {
-                                if (currentIndex <
-                                    onboardingContentList.length - 1) {
+                                if (currentIndex < onboardingContentList.length - 1) {
                                   _controller!.nextPage(
                                     duration: const Duration(milliseconds: 200),
                                     curve: Curves.easeInOut,
@@ -245,10 +242,8 @@ class _OnboardingContainerState extends State<OnboardingContainer> {
                                     width: 60.0,
                                     child: CircularProgressIndicator(
                                       value: percentage,
-                                      backgroundColor:
-                                          AppColors.primary.withOpacity(0.2),
-                                      valueColor:
-                                          const AlwaysStoppedAnimation<Color>(
+                                      backgroundColor: AppColors.primary.withOpacity(0.2),
+                                      valueColor: const AlwaysStoppedAnimation<Color>(
                                         AppColors.primary,
                                       ),
                                     ),
